a3830489b9fbd09cdaaa9af74fde82154e834cc5
andreaa21 <andrea.zollini@proton.me>
2023-01-23 17:35:54 +0100

    add category table

--- resources/views/admin/projects/category_projects_list.blade.php
+++ resources/views/admin/projects/category_projects_list.blade.php
@@ -1,3 +1,26 @@
-    @foreach ($categories as $category)
-        <h3>{{ $category->name }} </h3>
-    @endforeach
+<div class="container">
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Categoria</th>
+                <th scope="col">Progetto</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($categories as $category)
+                <tr>
+                    <td>{{ $category->name }}</td>
+                    <td>
+                        <ul>
+                            @foreach ($category->projects as $projects)
+                                <li>
+                                    {{ $projects->name }}
+                                </li>
+                            @endforeach
+                        </ul>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
